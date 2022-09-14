c09db50408e69567eb77af537be72c9fa0cf3488
Grace Chung <gracehojung@hotmail.com>
2022-09-13 23:42:34 -0400

    commited changes

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
-const { PORT = 5000 } = process.env;
-
-const app = require("./app");
 const knex = require("./db/connection");
+const express = require('express')
+const app = require('./app.js')
+const router = express.Router()
+const PORT = process.env.PORT || 5000;
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
+app.use('/', router);
 
 knex.migrate
   .latest()
-  .then((migrations) => {
-    console.log("migrations", migrations);
-    app.listen(PORT, listener);
+  .then((migrations)=> {
+    console.log("migrations", migrations[0])
+   
+    app.listen(PORT, () => console.log(`Server running on ${PORT} `))
   })
   .catch(console.error);
+module.exports = app
